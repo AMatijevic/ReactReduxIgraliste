1970f4553f6b71c7eecf4d77e452fdd400e3cea7
NETVACATION\andrej.matijevic <andrej.matijevic@dreamlines.de>
2017-06-08 16:52:42 +0200

    Added final webpack config

--- ReactIgraliste/ReactIgraliste/webpack.config.js
+++ ReactIgraliste/ReactIgraliste/webpack.config.js
@@ -1,17 +1,18 @@
 ﻿const webpack = require('webpack');
 const path = require('path');
+const bundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+//const assets = path.join(__dirname, "wwwroot");
 
 module.exports = {
     context: path.resolve(__dirname, './frontend'),
     //entry: "./frontend/index.tsx",
     entry: {
-        app: './index.tsx',
-        vendor: ['react', 'react-dom']
+        app: './index.tsx'
     },
     //__dirname refers to the directory where this webpack.config.js lives, which in this blogpost is the project root
     output: {
         filename: "[name].bundle.js",
-        path: __dirname + "/wwwroot/dist"
+        path: __dirname + "/wwwroot/dist" 
     },
     // Enable sourcemaps for debugging webpack's output.
     devtool: "source-map",
@@ -29,5 +30,14 @@ module.exports = {
             // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
             { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
         ]
-    }
+    },
+    plugins: [
+        new webpack.DllReferencePlugin({
+            context: ".",
+            manifest: require("./wwwroot/assets/vendor-manifest.json")
+        }),
+        new bundleAnalyzerPlugin({
+            analyzerMode: 'static'
+        })
+    ]
 };
---
+++ ReactIgraliste/ReactIgraliste/webpack.config.vendor.js
@@ -0,0 +1,32 @@
+const webpack = require('webpack');
+const path = require('path');
+//const bundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const assets = path.join(__dirname, "wwwroot", "assets");
+
+module.exports = {
+    //context: path.resolve(__dirname, './frontend'),
+    //entry: "./frontend/index.tsx",
+    entry: {
+        //app: './index.tsx',
+        vendor: ['react', 'react-dom']
+    },
+    //__dirname refers to the directory where this webpack.config.js lives, which in this blogpost is the project root
+    output: {
+        path: assets,
+        filename: "[name].js",
+        library: "[name]_dll"
+    },
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
+
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".ts", ".tsx", ".js", ".json"]
+    },
+    plugins: [
+        new webpack.DllPlugin({
+            path: path.join(assets, "[name]-manifest.json"),
+            name: '[name]_dll'
+        })
+    ]
+};
