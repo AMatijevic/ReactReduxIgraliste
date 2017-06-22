18e5e4da5fe0601648bba4269e8c0c93a6741c64
Andrej Matijevic <andrej.matijevic@gmail.com>
2017-06-22 09:26:36 +0200

    Started with counter component

---
+++ ReactIgraliste/ReactReduxTs./frontend/components/header.tsx
@@ -0,0 +1,12 @@
+﻿import * as React from "react";
+
+interface IHeaderProps {
+    compiler: string;
+    framework: string;
+}
+//Function component
+const Header = (props: IHeaderProps) => {
+    return (<h1> Hello from {props.compiler} and {props.framework}! </h1>);
+}
+
+export default Header;
---
+++ ReactIgraliste/ReactReduxTs/frontend/components/counter.tsx
@@ -0,0 +1,23 @@
+﻿import * as React from "react";
+import Header from "./header";
+
+interface ICounterProps {
+    //Ovdje idu cisto stvari samo za prikaz
+    //Propertiji propsa se nece mjenjati tokom zivota komponente
+    //Niti korinik niti sustav ih nece mjenjati
+
+}
+interface ICounterState {
+    //Nesto sto se mijenja kroz vrijeme nesto sa cime komponenta zapravo radi sto ce se mijenjati tokom zivota komponente
+    //Tu idu stvari koje bi stavio na scope u angularu da ih mozes bind na kontrolu jer ti njihove vrijednosti trebaju tokom zivota komponente
+    counter: number;
+}
+
+export class Counter extends React.Component<ICounterProps, ICounterState>{
+    render() {
+        return <div>
+            <Header compiler={'Text'} framework={'Text'}></Header>
+            <p>Counter</p>
+        </div>;
+    }
+}
