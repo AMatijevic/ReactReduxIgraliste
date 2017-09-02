4b7ba2368996c6b53163d1703ba8b982b387c8f4
Andrej <pixi00004@yahoo.com>
2017-09-02 20:56:17 +0200

    TypeScriptTemplate

---
+++ ReactIgraliste/ReactReduxTemplate/ClientApp/boot-client.tsx
@@ -0,0 +1,20 @@
+﻿import './css/site.css';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import {Hello} from './components/HelloWorld';
+
+
+function renderApp(){
+    ReactDOM.render(
+        <Hello />,
+        document.getElementById("react-app")
+    );
+}
+
+renderApp();
+
+if (module.hot) {
+    module.hot.accept('./components/HelloWorld', () => {
+        renderApp();
+    });
+}
